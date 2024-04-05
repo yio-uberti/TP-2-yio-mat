9698ee32727da259243ad40625525563c472fd8b
Mateo Gariboglio <63440714+Mateo-404@users.noreply.github.com>
2024-04-04 23:24:26 -0300

    Ejercicio-1_error
    Falta arreglar la funcion, tengo un error con el array

---
+++ Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ejercicio_1
+{
+    internal class Program
+    {
+        public static string invertirCifras(string palabra) {
+            char[] palabra_invertida =  {'0'};
+            uint contador = 0; 
+
+            for (int i = palabra.Length; i > 0; i--)
+            {
+                palabra_invertida[contador] = palabra[i];
+                contador++;
+            }
+            return Convert.ToString(palabra_invertida);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el numero a invertir: ");
+            String input = Console.ReadLine();
+
+            Console.WriteLine($"{invertirCifras(input)}");
+            
+            
+        }
+    }
+}
