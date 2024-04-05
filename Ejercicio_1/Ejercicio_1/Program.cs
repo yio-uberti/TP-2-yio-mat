663adde2dd1828d2428d0b6452f8655df685a26a
Mateo Gariboglio <63440714+Mateo-404@users.noreply.github.com>
2024-04-05 02:06:41 -0300

    Arreglo error

---
+++ Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ejercicio_1
+{
+    internal class Program
+    {
+        public static int invertirCifras(string palabra) {
+            
+            int resultado = 0, numero;
+            numero = int.Parse(palabra);
+
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                //Asigno el último dígito
+                resultado += numero % 10;
+                
+                //Descarto el último dígito
+                numero /= 10;
+    
+                //Corro la cifra de lugar hacia la izquiera
+                resultado *= 10;
+            }
+
+            return resultado/10;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el numero a invertir: ");
+            String input = Console.ReadLine();
+            Console.WriteLine($"{invertirCifras(input)}");
+            
+            
+        }
+    }
+}
