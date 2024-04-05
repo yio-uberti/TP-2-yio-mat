67a684fa0bcfa2956829988206fd2b00f20e2fa8
Mateo Gariboglio <63440714+Mateo-404@users.noreply.github.com>
2024-04-05 02:04:52 -0300

    Ejercicio_2
    Ejercicio finalizado

--- Ejercicio_2/Ejercicio_2/Program.cs
+++ Ejercicio_2/Ejercicio_2/Program.cs
@@ -1,38 +1,39 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System.ComponentModel;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Ejercicio_2
 {
     internal class Program
     {
         static bool primo(int num) {
-            
             if (num <= 1)
-                return false;
-            if(num <= 3)
-                return true;
-            if (num % 2 == 0 || num % 3 == 0)
-                return false;
-            for (int i = 5; i * i <= num; i += 6)
             {
-                if (num % i == 0 || num % (i + 2) == 0)
-                    return false;
+                return false;
             }
-            
+
+            // Cualquier numero Entero puede ser divisible por 1 y por si mismo, a excepcion del 0
+            for(uint i = 2; i < (num-1); i++){
+                //Si encuentra algún numero que puede ser dividido por otro numero rompe el ciclo
+                if ((num%i)==0)
+                {
+                    return false;
+                }
+            }
             return true;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Ingrese un numero");
-            int input = int.Parse(Console.ReadLine()); //leer
+            Console.WriteLine("Ingrese un numero: ");
+            int input = int.Parse(Console.ReadLine());
             Console.WriteLine();
             if (primo(input))
             {
-                Console.WriteLine("Es numero primo");
+                Console.WriteLine($"El numero \"{input}\" ES numero primo");
             }
             else
             {
-                Console.WriteLine("No es numero primo");
+                Console.WriteLine($"El numero \"{input}\" NO es numero primo");
             }
 
         }
